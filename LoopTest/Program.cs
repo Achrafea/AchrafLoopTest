560c19c94ed74f55d5ab19e7f58b0b7aab95ae14
Achraf EA <achraf.elaissaouali@hotmail.com>
2021-10-21 15:13:19 +0200

    Add project files.

---
+++ LoopTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int GrGetal = 0;
+            int Getal = 0;
+
+            while (i<=2)
+            {
+                Console.WriteLine("voer een getal in");
+
+                Getal = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+
+                if (i == 0)
+                {
+                    GrGetal = Getal;
+                }
+                if (Getal > GrGetal)
+                {
+                    GrGetal = Getal;  
+                }
+                i++;
+
+            }
+            Console.WriteLine($"Het grootste getal is{GrGetal}");
+
+
+        }
+    }
+}
---
+++ Oefening2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string naam = "Achraf";
+            int Engels = 8;
+            int ProNet = 9;
+            int gemiddelde;
+            int totaal;
+            int i = 0;
+            int x = 1;
+
+            bool work = true;
+
+            totaal = Engels + ProNet;
+            gemiddelde = totaal /2;
+
+            Console.WriteLine("Do you want to continue?");
+            string input = Console.ReadLine();
+
+            Console.WriteLine($"Naam student is: {naam}");
+            Console.WriteLine($"Engels: {Engels}");
+            Console.WriteLine($"ProNet: {ProNet}");
+            Console.WriteLine($"Het gemiddelde is: {gemiddelde}");
+
+            
+
+            do
+            {
+                
+                if (input == "n")
+                {
+                    work = false;
+                }
+
+            } while (work);
+
+        }
+    }
+}
